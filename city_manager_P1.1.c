5b436222bb05b4b6863afe9f73278eb96defcf31
maraciupa <ciupamara@gmail.com>
2026-04-26 19:53:10 +0300

    commit - retusuri functii

--- city_manager_P1.1.c
+++ city_manager_P1.1.c
@@ -161,29 +161,32 @@ void print_permission(mode_t mode)
 
 //Operations(commands) //
 
-void add_report(char *district_id, char *role, char *user)
+void add(char *district_id)
 {
-	char fisier_sursa[500];
+	char p[500]; //path
 	
-	strcpy(fisier_sursa, district_id);
-	strcat(fisier_sursa, "/");
-	strcat(fisier_sursa, "reports.dat");
+	strcpy(p, district_id);
+	strcat(p, "/");
+	strcat(p, "reports.dat");
 
-	permission_verify(fisier_sursa, role, 'w');
-	int fd=open(fisier_sursa, O_WRONLY | O_CREAT | O_APPEND, 0664);
+	
+	int fd=open(p, O_WRONLY | O_CREAT | O_APPEND, 0664);
 	if(fd==-1)
 		{
 			perror("Error! reports.dat cannot be opened!");
 			exit(-1);
 		}
-	chmod(fisier_sursa, 0664);
+
+	chmod(p, 0664);
 	Report r;
 	srand(time(NULL));
 	r.id=rand()%100000;
-	strcpy(r.inspector_name, user);
 	r.timestamp=time(NULL);
 
 	printf("REPORT:\n");
+	//reading the name of the inspector:
+	printf("Inspector name: ");
+	scanf("%s", r.inspector_name);
 	printf("Enter the GPS coordinates:\n");
 	printf("Latitude: ");
 	scanf("%f", &r.lat);
@@ -216,21 +219,21 @@ void add_report(char *district_id, char *role, char *user)
 }
 
 
-void list(char *district_id, char *role)
+void list(char *district_id)
 {
-	char fisier_sursa[500];
-	strcpy(fisier_sursa, district_id);
-	strcat(fisier_sursa, "/reports.dat");
+	char p[500]; //path
+	strcpy(p, district_id);
+	strcat(p, "/reports.dat");
 	
-	permission_verify(fisier_sursa, role, 'r');
-
+	
 	struct stat st;
 	//using the stat() function:
-	if(stat(fisier_sursa, &st)==-1)
+	if(stat(p, &st)==-1)
 	  {
 		perror("Error! Couldn't get the data!");
 		exit(-1);
 	   }
+
 	
 	printf("FILE DATA:\n");
 	printf("Permissions:\n");
@@ -239,7 +242,7 @@ void list(char *district_id, char *role)
 	printf("File size: %lld bytes; Last Modified: %s ", st.st_size, ctime(&st.st_mtime));
 	printf("\n");
 	
-	int fd=open(fisier_sursa, O_RDONLY);
+	int fd=open(p, O_RDONLY);
 	if(fd==-1)
 	{
 		perror("Error! couldn't read from file!");
@@ -249,6 +252,7 @@ void list(char *district_id, char *role)
 	Report r;
 	int nr=0;
 
+	//reading every report until the end:
 	while(read(fd, &r, sizeof(Report))>0)
 	{
 		nr++;
@@ -274,15 +278,14 @@ void list(char *district_id, char *role)
 }
 
 
-void view(char *district_id, char *role, int wanted_id)
+void view(char *district_id, int report_id)
 {
-	char fisier_sursa[500];
-	strcpy(fisier_sursa, district_id);
-	strcat(fisier_sursa, "/reports.dat");
+	char p[500]; //path
+	strcpy(p, district_id);
+	strcat(p, "/reports.dat");
 	
-	permission_verify(fisier_sursa, role, 'r');
 	
-	int fd=open(fisier_sursa, O_RDONLY);
+	int fd=open(p, O_RDONLY);
 	if(fd==-1)
 	{
 		perror("Error! couldn't read from file!");
@@ -294,9 +297,10 @@ void view(char *district_id, char *role, int wanted_id)
 	
 	while(read(fd, &r, sizeof(Report))>0)
 	{
-		if(r.id==wanted_id)
+		if(r.id==report_id)
 		{
 			ok=1;
+			printf("REPORT # %d:\n", report_id);
 			printf("Inspector Name: ");
 			printf("%s\n", r.inspector_name);
 			printf("Coordinates: ");
@@ -321,8 +325,151 @@ void view(char *district_id, char *role, int wanted_id)
 
 }
 
+void remove_report(char *district_id, int wanted_id)
+{
+	
+	char p[500]; //path
+	strcpy(p, district_id);
+	strcat(p, "/reports.dat");
 
-int main(int argc, char *argv[])
+	int fd=open(p, O_RDWR);
+	if(fd==-1)
+	{
+		perror("Error! couldn't open file!");
+		exit(-1);
+	}
+
+	Report r;
+	int ok=0;
+	int poz=0;
+	
+	//searching for the wanted report and finding the position for it:
+	while(read(fd, &r, sizeof(Report))>0)
+	{
+		if(r.id==wanted_id)
+		{
+			ok=1;
+			poz=lseek(fd, 0, SEEK_CUR)-sizeof(Report);
+			break;
+		}
+	}
+
+	if(ok==0)
+	{
+		printf("Error! report not found!");
+		exit(-1);
+	}
+	
+	//shifting the following reports a position to the left:
+	Report next_report;
+	long current_poz=poz+sizeof(Report);
+	
+	while(lseek(fd, current_poz, SEEK_SET)!=-1 && read(fd, &next_report, sizeof(Report))>0)
+	{
+		lseek(fd, current_poz-sizeof(Report), SEEK_SET);
+		write(fd, &next_report, sizeof(Report));
+		current_poz=current_poz+sizeof(Report);
+	}
+
+	struct stat st;
+	fstat(fd, &st);
+	if(ftruncate(fd, st.st_size-sizeof(Report))==-1)
+	{
+		perror("Error! couldn't truncate the file!");
+	}
+	else
+	{
+		printf("Truncate: successfully finished!");
+	}
+
+	close(fd);
+}
+
+
+void update_threshold(char *district_id, int value)
+{
+	int ok=0;
+	char p[500];
+	sprintf(p, "%s/district.cfg", district_id);
+
+	struct stat st;
+	if(stat(p, &st)==0)
+	{
+		/*
+		if((st.st_mode & 0777)!=0640)
+		{
+			printf("Permission denied! It is not 640!");
+			return;
+		}*/
+		
+		//Verify Owner: rw
+		if((st.st_mode & S_IRUSR)==0) 
+		{
+			ok=0;
+		}
+		if((st.st_mode & S_IWUSR)==0) 
+		{
+			ok=0;
+		}
+		if((st.st_mode & S_IXUSR)!=0) 
+		{
+			ok=0;
+		}
+
+		//Verify Group: r
+		if((st.st_mode & S_IRGRP)==0) 
+		{
+			ok=0;
+		}
+		if((st.st_mode & S_IWGRP)!=0) 
+		{
+			ok=0;
+		}
+		if((st.st_mode & S_IXGRP)!=0) 
+		{
+			ok=0;
+		}
+
+		//Verify Others: -
+		if((st.st_mode & S_IROTH)==0) 
+		{
+			ok=0;
+		}
+		if((st.st_mode & S_IWOTH)!=0) 
+		{
+			ok=0;
+		}
+		if((st.st_mode & S_IXOTH)!=0) 
+		{
+			ok=0;
+		}
+		
+	}
+
+	int fd=open(p, O_WRONLY | O_CREAT | O_TRUNC, 0640);
+	if(fd==-1)
+	{
+		perror("Error! couldn't open the file!");
+		exit(-1);
+	}
+	
+	char value_to_string[10];
+	sprintf(value_to_string, "%d", value);
+	if((write(fd, value_to_string, strlen(value_to_string)))==-1)
+	{
+		perror("Error! couldn't write in the file!");
+		exit(-1);
+	}
+	else
+	{
+		printf("Threshold was successfully added to %d!", value);
+	}
+	close(fd);
+
+}
+
+
+int main(int argc, char **argv)
 {
      	if(argc<5)
 		{
@@ -335,6 +482,7 @@ int main(int argc, char *argv[])
 	
 	if(strcmp(argv[1], "--role")==0)
 	{
+		
 		strcpy(role, argv[2]);
 	}
 
@@ -342,19 +490,60 @@ int main(int argc, char *argv[])
 	{
 		strcpy(district, argv[4]);
 		create_district_directory(district);
-		add_report(district, role, user);
+		//creating the path for the permission_verify function:
+		char p[500];
+		sprintf(p, "%s.reports.dat", district);
+		permission_verify(p, role, 'w');
+		add(district);
 	}
 	else if(strcmp(argv[3], "--list")==0)
 	{
 		strcpy(district, argv[4]);
-		list(district, role);
+		//creating the path:
+		char p[500];
+		sprintf(p, "%s/reports.dat", district);
+		permission_verify(p, role, 'r');
+		list(district);
 	}
 	else if(strcmp(argv[3], "--view")==0)
 	{
 		strcpy(district, argv[4]);
-		int wanted_id=atoi(argv[5]);
-		view(district, role, wanted_id);
+		int wanted_id=atoi(argv[5]); //the wanted report id
+		//creating the path:
+		char p[500];
+		sprintf(p, "%s/reports.dat", district);
+		permission_verify(p, role, 'r');
+		view(district, wanted_id);
 	}
+	else if(strcmp(argv[3], "--view")==0)
+	{
+		strcpy(district, argv[4]);
+		int wanted_id=atoi(argv[5]); //the wanted report id
+		//creating the path:
+		char p[500];
+		sprintf(p, "%s/reports.dat", district);
+		permission_verify(p, role, 'r');
+		view(district, wanted_id);
+	}
+	else if(strcmp(argv[3], "--remove_report")==0)
+	{
+		if(strcmp(role, "manager")!=0)
+		{
+			printf("Error! Remove_report is for managers only!");
+			exit(-1);
+		}
+		else
+		{
+			strcpy(district, argv[4]);
+			int wanted_id=atoi(argv[5]);
+			//creating the path:
+			char p[500];
+			sprintf(p, "%s/reports.dat", district);
+			permission_verify(p, role, 'w');
+			remove_report(district, wanted_id);
+		}
+	}
+
 	else if(strcmp(argv[3], "--user")==0)
 	{
 		strcpy(user, argv[4]);
@@ -362,21 +551,65 @@ int main(int argc, char *argv[])
 		{
 			strcpy(district, argv[6]);
 			create_district_directory(district);
-			add_report(district, role, user);
+			//creating the path:
+			char p[500];
+			sprintf(p, "%s/reports.dat", district);
+			permission_verify(p, role, 'w');
+			add(district);
 		}
 		else if(strcmp(argv[5], "--list")==0)
 		{
 			strcpy(district, argv[6]);
-			list(district, role);
+			//creating the path:
+			char p[500];
+			sprintf(p, "%s/reports.dat", district);
+			permission_verify(p, role, 'r');
+			list(district);
 		}
 		else if(strcmp(argv[5], "--view")==0)
 		{
 			strcpy(district, argv[6]);
 			int wanted_id=atoi(argv[7]);
-			view(district, role, wanted_id);
+			//creating the path:
+			char p[500];
+			sprintf(p, "%s/reports.dat", district);
+			permission_verify(p, role, 'r');
+			view(district, wanted_id);
+		}
+		else if(strcmp(argv[3], "--remove_report")==0)
+		{
+			if(strcmp(role, "manager")!=0)
+			{
+				printf("Error! Remove_report is for managers only!");
+				exit(-1);
+			}
+			else
+			{
+				strcpy(district, argv[6]);
+				int wanted_id=atoi(argv[7]);
+				//creating the path:
+				char p[500];
+				sprintf(p, "%s/reports.dat", district);
+				permission_verify(p, role, 'w');
+				remove_report(district, wanted_id);
+			}
+		}
+		else if(strcmp(argv[5], "--update_threshold")==0)
+		{
+			if(strcmp(role, "manager")!=0)
+			{
+				printf("Error! Update_threshold is for managers only!");
+				exit(-1);
+			}
+			else
+			{
+				strcpy(district, argv[6]);
+				int val=atoi(argv[7]);
+				update_threshold(district, val);
+			}
 		}
 		
-	}
+		}
 	else
 	{
 		printf("Error! invalid command!\n");
